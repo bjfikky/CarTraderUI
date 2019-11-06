815ca8efa8ac119f30a0aae910d3e42396c3de35
Benjamin Orimoloye <borimoloye@HQ.Boatus.com>
2019-11-06 12:48:16 -0500

    Added login and signup form template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ListingsComponent} from './components/listing/listings/listings.component';
+import {LoginSignupComponent} from './components/login-signup/login-signup.component';
+import {ListingDetailComponent} from './components/listing/listing-detail/listing-detail.component';
+import {ListingFormComponent} from './components/listing/listing-form/listing-form.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'listings', pathMatch: 'full' },
-  { path: 'listings', component: ListingsComponent }
+  { path: 'listings', component: ListingsComponent },
+  { path: 'listings/edit/:listingId', component: ListingFormComponent },
+  { path: 'listings/new', component: ListingFormComponent },
+  { path: 'listings/:listingId', component: ListingDetailComponent },
+  { path: 'account', component: LoginSignupComponent },
+  { path: '**', component: ListingsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {BsDropdownModule} from 'ngx-bootstrap';
+import {BsDropdownModule, TabsModule} from 'ngx-bootstrap';
 import { LoginSignupComponent } from './components/login-signup/login-signup.component';
 import { ListingsComponent } from './components/listing/listings/listings.component';
+import { LoginComponent } from './components/login-signup/login/login.component';
+import { SignupComponent } from './components/login-signup/signup/signup.component';
+import { ListingDetailComponent } from './components/listing/listing-detail/listing-detail.component';
+import { ListingFormComponent } from './components/listing/listing-form/listing-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     LoginSignupComponent,
-    ListingsComponent
+    ListingsComponent,
+    LoginComponent,
+    SignupComponent,
+    ListingDetailComponent,
+    ListingFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
